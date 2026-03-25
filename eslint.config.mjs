80cbfa89239d2db7ba7c301b2c4aecc77a88c1e5
Ismo Vuorinen <ismo@ivuorinen.net>
2026-03-25 19:16:16 +0200

    build: migrate to ESM and simplify Babel config
    Strip legacy Babel proposal plugins in favor of preset-env with
    modules:false. Convert jest.setup.js and recommended.js to ESM.
    Add ESLint flat config (eslint.config.mjs) and ESM Jest config.

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import jest from 'eslint-plugin-jest';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  jest.configs['flat/recommended'],
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        testRule: 'readonly',
+        console: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+      },
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          singleQuote: true,
+          trailingComma: 'es5',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '.coverage/**'],
+  },
+];
